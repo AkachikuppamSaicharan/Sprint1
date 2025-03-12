d2de78d57468c429da4623070fd60a15ce00d928
AkachikuppamSaiCharan <akachikuppamsaicharan@gmail.com>
2025-03-12 19:23:40 +0530

    Minor Changes to US002 Which is Different

---
+++ US002_Sprint_2.sql
@@ -0,0 +1,13 @@
+CREATE TABLE US002_Customer(
+    CustomerNumber NUMERIC(13) PRIMARY KEY,
+    CustomerName VARCHAR(50) NOT NULL,
+    Email VARCHAR(250) NOT NULL UNIQUE,
+    MobileNumber NUMERIC(10) NOT NULL UNIQUE
+);
+CREATE TABLE BillDetails(
+    BillNo NUMERIC(5) PRIMARY KEY,
+    PayableAmount NUMERIC(7,2) NOT NULL,
+    DueAmount NUMERIC(7,2) NOT NULL,
+    CustomerNumber NUMERIC(13),
+    Constraint Fkey FOREIGN KEY(CustomerNumber) REFERENCES US002_Customer(CustomerNumber)
+);
--- src/Sprint_2_US002_Bill_Details_JDBC.java
+++ src/Sprint_2_US002_Bill_Details_JDBC.java
@@ -4,24 +4,18 @@ import java.util.Scanner;
 public class Sprint_2_US002_Bill_Details_JDBC {
     static Scanner Sc=new Scanner(System.in);
     public static void main(String[] args) throws SQLException {
-        CreateBillTable();
         CreateCustomerTable();
+        CreateBillTable();
         DropTable();
 
     }
     public static void CreateCustomerTable() throws SQLException {
         Connection Con= DriverManager.getConnection("jdbc:derby:AkachikuppamSaiCharan_ElectricityManagement");
-        String CreateTable="CREATE TABLE Sprint2_US002_Customer(\n" +
-                "     ConsumerId NUMERIC(13) PRIMARY KEY,\n" +
-                "     BillNo NUMERIC(5) NOT NULL UNIQUE,\n" +
-                "     Title VARCHAR(3) NOT NULL,\n" +
-                "     CustomerName VARCHAR(50) NOT NULL,\n" +
-                "     Email VARCHAR(250) NOT NULL UNIQUE,\n" +
-                "     MobileNo NUMERIC(10) NOT NULL UNIQUE,\n" +
-                "     UserId VARCHAR(20) NOT NULL UNIQUE,\n" +
-                "     Password VARCHAR(30) NOT NULL,\n" +
-                "     ConfirmPassword VARCHAR(30) NOT NULL,\n" +
-                "    CONSTRAINT Fkey FOREIGN KEY (BillNo) REFERENCES BillDetails(BillNo)\n" +
+        String CreateTable="CREATE TABLE US002_Customer(\n" +
+                "    CustomerNumber NUMERIC(13) PRIMARY KEY,\n" +
+                "    CustomerName VARCHAR(50) NOT NULL,\n" +
+                "    Email VARCHAR(250) NOT NULL UNIQUE,\n" +
+                "    MobileNumber NUMERIC(10) NOT NULL UNIQUE\n" +
                 ")";
 
         PreparedStatement Stmt=Con.prepareStatement(CreateTable);
@@ -37,8 +31,10 @@ public class Sprint_2_US002_Bill_Details_JDBC {
         Connection Con= DriverManager.getConnection("jdbc:derby:AkachikuppamSaiCharan_ElectricityManagement");
         String CreateBillTable="CREATE TABLE BillDetails(\n" +
                 "    BillNo NUMERIC(5) PRIMARY KEY,\n" +
-                "    DueAmount DECIMAL(7,2) NOT NULL,\n" +
-                "    PayableAmount DECIMAL(7,2) NOT NULL\n" +
+                "    PayableAmount NUMERIC(7,2) NOT NULL,\n" +
+                "    DueAmount NUMERIC(7,2) NOT NULL,\n" +
+                "    CustomerNumber NUMERIC(13),\n" +
+                "    Constraint Fkey FOREIGN KEY(CustomerNumber) REFERENCES US002_Customer(CustomerNumber)\n" +
                 ")";
         PreparedStatement Stmt=Con.prepareStatement(CreateBillTable);
         int nR=Stmt.executeUpdate();
@@ -53,12 +49,12 @@ public class Sprint_2_US002_Bill_Details_JDBC {
     }
     public static void DropTable() throws SQLException {
         Connection Con= DriverManager.getConnection("jdbc:derby:AkachikuppamSaiCharan_ElectricityManagement");
-        String DROPTable="DROP TABLE Sprint2_US002_Customer";
+        String DROPTable="DROP TABLE US002_Customer";
         String DROPBillTable="DROP TABLE BillDetails";
         PreparedStatement Stmt=Con.prepareStatement(DROPTable);
         PreparedStatement Stmt1=Con.prepareStatement(DROPBillTable);
-        Stmt.execute();
         Stmt1.execute();
+        Stmt.execute();
         Stmt.close();
         Stmt1.close();
         Con.close();
